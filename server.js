768cc732872b08a5649ae814974ad1e43c7ccbf8
KPorus <designerfardin2021@gmail.com>
2024-10-30 11:44:33 +0600

    database connectivity solved

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.post('/NEXTINLINE', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
 });
